9f17efb64e14eea156323fc3dc4cc5d4bb5e46f5
Connie Standish <conniestand@gmail.com>
2022-07-31 12:57:05 -0400

    Deleted pseudocode

--- index.js
+++ index.js
@@ -48,10 +48,3 @@ getPoemBtn.onclick = async function() {
   // renders the HTML string returned by makePoemHTML to #poem
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
 }
-
-//Write code in the makePoemHTML function
-//output a single string of HTML inside #poem div
-//string should have h2, em inside h3, and p tags for each stanza
-//p tags should contain linebreak tags
-//use the makeTag and pipe starter code
-//split, join, and map can help separate poem into stanzas
